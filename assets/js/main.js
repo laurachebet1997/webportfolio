fae55f4943c1caad8de5571923c5d157edece372
laurachebet1997 <rosechebet19971997@gmail.com>
2023-05-28 17:53:53 +0300

    footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,39 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.getElementById('nav-menu'),
+const navMenu = document.getElementById('tog'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
+/*if(navToggle){
     navToggle.addEventListener('click', ()=> {
     navMenu.classList.add('show-menu')})
-}
+}*/
+navToggle.addEventListener('click',()=>{
+    if(navMenu.style.display === 'none'){
+        navMenu.style.display = 'block';
+    }
+    else{
+        navMenu.style.display = 'none';
+    }
+})
 
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
+/*if(navClose){
     navClose.addEventListener('click', ()=> {
     navMenu.classList.remove('show-menu')})
-}
+}*/
+navClose.addEventListener('click',()=>{
+    if(navMenu.style.display === 'block'){
+        navMenu.style.display = 'none';
+    }
+    else{
+        navMenu.style.display = 'block';
+    }
+})
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
@@ -41,8 +57,8 @@ function toggleSkills(){
     for(i=0;i<skillsContent.length;i++){
         skillsContent[i].className = 'skills__content skills__close'
     }
-    if(itemClass='skill__content skill__close'){
-        this.parentNode.className = 'skill__content skill__open'
+    if(itemClass='skills__content skill__close'){
+        this.parentNode.className = 'skills__content skill__open'
     }
 }
 
@@ -51,8 +67,8 @@ skillsHeader.forEach((el)=>{
 })
 
 /*==================== QUALIFICATION TABS ====================*/
-const tabs = document.querySelectorAll('[data-target'),
-      tabContents = document.querySelectorAll('[data-content')
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
 
 tabs.forEach(tab=>{
     tab.addEventListener('click', ()=>{
@@ -68,19 +84,71 @@ tabs.forEach(tab=>{
 })
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+modalBtns.forEach((modalBtn, i)=>{
+    modalBtn.addEventListener('click',()=>{
+        modal(i)
+    })
+})
+modalCloses.forEach((modalClose)=>{
+    modalClose.addEventListener('click',()=>{
+        modalViews.forEach((modalView)=>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
-
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/ 
 
